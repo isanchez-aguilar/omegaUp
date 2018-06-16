af6c068fc6ff8200fc20acac1f5752b640e025b3
isanchez-aguilar <saais31@gmail.com>
2018-06-16 12:57:23 -0500

    Add files via upload
    omegaUp Wasted como el Adonais Solution

---
+++ Wasted como el Adonais.cpp
@@ -0,0 +1,23 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: omegaUp Wasted como el Adonais
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << "abaababaabaababaababaabaababaabaababaababaabaababa\n";
+	cout << "ababaabaababaabaababaababaabaababaabaababaababaaba\n";
+	cout << "ababaababaabaababaabaababaababaabaababaababaabaaba\n";
+	cout << "baabaababaababaabaababaabaababaababaabaababaababaa\n";
+	cout << "baababaabaababaababaabaababaabaababaababaabaababaa\n";
+	cout << "babaabaababaabaababaababaabaababaababaabaababaabaa\n";
+	cout << "babaababaabaababaabaababaababaabaababaababaabaabab\n";
+
+	return 0;
+}
