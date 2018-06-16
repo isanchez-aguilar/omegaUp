9602256957a05c54f9b9f7b89ed8f5a6dd7b0fd3
isanchez-aguilar <saais31@gmail.com>
2018-06-16 13:10:06 -0500

    Add files via upload
    omegaUp On ta el pinche fácil pa irme? solution

---
+++ On ta el pinche fácil pa irme.cpp
@@ -0,0 +1,21 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: omegaUp On ta el pinche fácil pa irme?
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	unsigned long long n;
+	unsigned long long m;
+	cin >> n >> m;
+
+	cout << n * m << "\n";
+
+	return 0;
+}
